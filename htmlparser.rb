cab45f6a1088eba058bc76a98c496ae54831c037
inamuu <8310973+inamuu@users.noreply.github.com>
2019-08-14 08:18:32 +0900

    :gem: オブジェクト指向で書き直し

--- htmlparser.rb
+++ htmlparser.rb
@@ -4,32 +4,42 @@ require 'nokogiri'
 require 'slack/incoming/webhooks'
 require 'dotenv'
 
-list = YAML.load_file("list.yaml")
+class Tw4HtmlParser
+  def initialize(site_url)
+    @list = site_url
+  end
 
-list['siteurl'].each do | url |
+  def site_parser
+    @list['siteurl'].each do | url |
+      doc = Nokogiri::HTML(open(url).read)
+      arr = Array.new
+      doc.xpath('//nav//li//a').each do | node |
+        arr << node
+      end
 
-  doc = Nokogiri::HTML(open(url).read)
-  arr = Array.new
-  doc.xpath('//nav//li//a').each do | node |
-    arr << node
-  end 
+      str = arr[0].to_s
+      @@notify_url = str.gsub(/<a href="|">|<\/a>/,' ')
+    end
+  end
 
-  str = arr[0].to_s
-  str2 = str.gsub(/<a href="|">|<\/a>/,' ')
-
-  Dotenv.load
-  slack = Slack::Incoming::Webhooks.new ENV["APIKEY"]
-
-  attachments = [{
-    color: "#483D8B",
-    fields: [
-      {
-      title: "妄想テレパシー最新話",
-      value: "#{str2}",
-      }
-    ],
-  }]
-
-  slack.post "", attachments: attachments
-
+  def notify_slack
+    Dotenv.load
+    slack = Slack::Incoming::Webhooks.new ENV["APIKEY"]
+    notify_url = @@notify_url
+    attachments = [{
+      color: "#483D8B",
+      fields: [
+        {
+        title: "妄想テレパシー最新話",
+        value: notify_url,
+        }
+      ],
+    }]
+    slack.post "", attachments: attachments
+  end
 end
+
+list = YAML.load_file("list.yaml")
+res = Tw4HtmlParser.new(list)
+res.site_parser
+res.notify_slack
